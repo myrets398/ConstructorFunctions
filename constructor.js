1220c306b60e871a091c75e3891ae08ba028ad98
myrets398 <myates102@gmail.com>
2016-09-28 14:40:07 -0600

    third try

---
+++ constructor.js
@@ -0,0 +1,11 @@
+//Contructer Functions
+
+//1. Create an array called bands with 3 objects in it. Each of them describes a music band. Each object should have a name, genre, numberOfPerformers, and numberOfGroupies.
+
+//Now create a constructor function called Band. Have it take in a name, genre, numberOfPerformers, and numberOfGroupies. Set the keys to have the value of the parameters using the this keyword.
+
+//Now create those same bands you made before using the constructor function Band using the new keyword. Plug in the values for each of the object keys as argumeters.
+
+//Now create a new array called newBands and push each of the objects that you made into that array.
+
+//Alright, you have created new objects using a constructor function. Let's say we want to add a new key to the constructor function. To do so, go ahead and create a method called bandInfo using the prototype method for the Band constructor. Let's have the bandInfo method alert the band's name and their music genre.
